434da688288e7de0d223dbbeca7f1f6f62f2d301
bodzash <bozzaylaszlo4@gmail.com>
2022-11-06 23:09:52 +0100

    feat: made a starting basic template

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
+import Join from './pages/Join';
+import Chat from './pages/Chat';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Join />} />
+    </Routes>
+  </BrowserRouter>
 );
---
+++ src/pages/Join.jsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from 'react';
+import io from 'socket.io-client'
+
+const socket = io.connect('http://localhost:8000');
+
+const sendMessage = (payload) => {
+  socket.emit('client_msg', payload);
+}
+
+const App = () => {
+  const [messages, setMessages] = useState([]);
+  const [input, setInput] = useState('');
+
+  useEffect(() => {
+    socket.on('server_msg', (stream) => {
+      console.log(stream);
+    });
+  }, [socket]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setInput('');
+    sendMessage(input);
+  }
+
+	return (
+    <div>
+      {/* Form */}
+      <form onSubmit={handleSubmit}>
+        <input placeholder='Write something...'
+          onChange={({target}) => setInput(target.value)}
+          value={input}
+        />
+        <button>Send</button>
+      </form>
+
+      {/* Messages holder shit */}
+      <div>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
