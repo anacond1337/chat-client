df8f45d7b44b8fbbe593648132ff7a3c13a31a44
anacond1337 <andrasarmand0419@gmail.com>
2022-11-07 00:26:25 +0100

    added uuid

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import Join from './pages/Join';
 import Chat from './pages/Chat';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Join />} />
+      <Route path="/" element={<Chat />} />
     </Routes>
-  </BrowserRouter>
+  </BrowserRouter>,
 );
--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,5 +1,48 @@
-const Chat = () => {
+import React, { useEffect, useState } from 'react';
+import io from 'socket.io-client';
+import { v4 as uuidv4 } from 'uuid';
 
+const socket = io.connect('http://localhost:8000');
+
+const sendMessage = (payload) => {
+  socket.emit('client_msg', payload);
 };
 
-export default Chat;
+function App() {
+  const [messages, setMessages] = useState([]);
+  const [input, setInput] = useState('');
+
+  useEffect(() => {
+    socket.on('server_msg', (stream) => {
+      setMessages((oldMessage) => [...oldMessage, stream]);
+    });
+  }, [socket]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setInput('');
+    sendMessage(input);
+  };
+
+  return (
+    <div>
+      {/* Form */}
+      <form onSubmit={handleSubmit}>
+        <input
+          placeholder="Write something..."
+          onChange={({ target }) => setInput(target.value)}
+          value={input}
+        />
+        <button type="submit">Send</button>
+      </form>
+
+      {/* Messages holder shit */}
+
+      <div>
+        {messages.map((mess) => <p key={uuidv4()}>{mess}</p>)}
+      </div>
+    </div>
+  );
+}
+
+export default App;
